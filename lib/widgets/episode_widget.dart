04e75f0ed9b178455c867441004c1315b3f4a5de
1day1 <onedayone@gmail.com>
2024-02-20 08:06:06 +0900

    url_launcher , iOS fix (cocoapods install)

--- lib/widgets/episode_widget.dart
+++ lib/widgets/episode_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 import 'package:webtoon/models/webtoon_episode_model.dart';
 
